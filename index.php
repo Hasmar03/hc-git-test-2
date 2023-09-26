62c1304b165642103608bb342e96aa614525716b
Hasmar03 <ashmarasmar081@gmail.com>
2023-09-27 01:21:09 +0800

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello Word!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque nisi, velit quaerat recusandae ipsum!</p>
 </body>
 </html>
